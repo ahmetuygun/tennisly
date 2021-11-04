9d6741fcb9a7e02ebc382d9593489886edf87083
DESKTOP-17A2KST\ahmet <ygnhmt@gmail.com>
2021-11-04 12:16:15 -0700

    Initial version of tennisly generated by generator-jhipster@7.3.1

---
+++ src/main/java/com/tennisly/club/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.tennisly.club.repository;
+
+import com.tennisly.club.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/tennisly/club/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.tennisly.club.web.rest.vm;
